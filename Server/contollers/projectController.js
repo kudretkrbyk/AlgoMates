b173826fca3f9c53d028f529b5eb935057b55bb1
kudretkrbyk <kudretkrbyk@gmail.com>
2025-04-19 20:57:42 +0300

    sanityfıunction eklendi

--- Server/contollers/projectController.js
+++ Server/contollers/projectController.js
@@ -1,4 +1,5 @@
 const Project = require("../models/projects");
+const { sanityFunction } = require("../middleware/sanityFunction");
 
 const getAll = async (req, res) => {
   try {
@@ -12,23 +13,25 @@ const getAll = async (req, res) => {
 const addData = async (req, res) => {
   try {
     let { title, tag, description, image, liveview, github } = req.body;
-    // const tag = sanityFunction(adiSoyadi);
-    // const description = sanityFunction(tcKimlikNo);
-    // const s_telefon = sanityFunction(telefon);
-    // const s_adres = sanityFunction(adres);
-    // const s_referans = sanityFunction(referans);
-    // if (
-    //   !s_adisoyadi ||
-    //   !s_tcKimlikNo ||
-    //   !s_telefon ||
-    //   !s_adres ||
-    //   !s_referans
-    // ) {
-    //   res.send({
-    //     success: false,
-    //     message: "lütfen geçerli veriler ile tekrar deneyiniz!!",
-    //   });
-    // }
+    const s_title = sanityFunction(title);
+    const s_tag = sanityFunction(tag);
+    const s_description = sanityFunction(description);
+    const s_image = sanityFunction(image);
+    const s_liveview = sanityFunction(liveview);
+    const s_github = sanityFunction(github);
+    if (
+      !s_title ||
+      !s_tag ||
+      !s_description ||
+      !s_image ||
+      !s_liveview ||
+      !s_github
+    ) {
+      res.send({
+        success: false,
+        message: "lütfen geçerli veriler ile tekrar deneyiniz!!",
+      });
+    }
 
     const response = await Project.create({
       title: title,
@@ -47,10 +50,10 @@ const addData = async (req, res) => {
 const getById = async (req, res) => {
   try {
     let id = req.params.id;
-    // const s_id = sanityFunction(id);
-    // if (!s_id) {
-    //   res.send({ success: false, message: "lütfen geçerli bir id giriniz" });
-    // }
+    const s_id = sanityFunction(id);
+    if (!s_id) {
+      res.send({ success: false, message: "lütfen geçerli bir id giriniz" });
+    }
 
     const response = await Project.findByPk(id);
     res.send(response);
@@ -61,26 +64,26 @@ const getById = async (req, res) => {
 const updateById = async (req, res) => {
   try {
     let id = req.params.id;
-    // const s_id = sanityFunction(id);
+    const s_id = sanityFunction(id);
     let { title, tag, description, image, liveview, github } = req.body;
-    // const s_adisoyadi = sanityFunction(adiSoyadi);
-    // const s_tcKimlikNo = sanityFunction(tcKimlikNo);
-    // const s_telefon = sanityFunction(telefon);
-    // const s_adres = sanityFunction(adres);
-    // const s_referans = sanityFunction(referans);
-    // if (
-    //   !s_id ||
-    //   !s_adisoyadi ||
-    //   !s_tcKimlikNo ||
-    //   !s_telefon ||
-    //   !s_adres ||
-    //   !s_referans
-    // ) {
-    //   res.send({
-    //     success: false,
-    //     message: "lütfen geçerli veriler ile tekrar deneyiniz!!",
-    //   });
-    // }
+    const s_adisoyadi = sanityFunction(adiSoyadi);
+    const s_tcKimlikNo = sanityFunction(tcKimlikNo);
+    const s_telefon = sanityFunction(telefon);
+    const s_adres = sanityFunction(adres);
+    const s_referans = sanityFunction(referans);
+    if (
+      !s_id ||
+      !s_adisoyadi ||
+      !s_tcKimlikNo ||
+      !s_telefon ||
+      !s_adres ||
+      !s_referans
+    ) {
+      res.send({
+        success: false,
+        message: "lütfen geçerli veriler ile tekrar deneyiniz!!",
+      });
+    }
 
     const response = await Project.update(
       {
@@ -110,10 +113,10 @@ const updateById = async (req, res) => {
 const deleteById = async (req, res) => {
   try {
     let id = req.params.id;
-    // const s_id = sanityFunction(id);
-    // if (!s_id) {
-    //   res.send({ success: false, message: "lütfen geçerli bir id giriniz" });
-    // }
+    const s_id = sanityFunction(id);
+    if (!s_id) {
+      res.send({ success: false, message: "lütfen geçerli bir id giriniz" });
+    }
 
     const response = await Project.destroy({
       where: { id: id },
